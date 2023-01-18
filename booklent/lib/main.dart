ceb9a1eb851780f58bf2db45d2403d7299902f8d
Jishna-tp <jishnatp7@gmail.com>
2023-01-18 22:03:19 +0530

    my commit

--- booklent/lib/main.dart
+++ booklent/lib/main.dart
@@ -1,115 +1,272 @@
+import 'package:booklent/forgot_pw.dart';
+import 'package:booklent/signup.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Login(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Login extends StatefulWidget {
+  const Login({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Login> createState() => _LoginState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _LoginState extends State<Login> {
+  bool? isremember = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          color: Color(0x3375F7FF),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Stack(
+                        children: [
+                          Align(
+                            widthFactor: 0.5,
+                            heightFactor: 0.5,
+                            alignment: Alignment.topRight,
+                            child: Container(
+                              height: 200,
+                              width: 200,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Color(0xE610F1FF)),
+                            ),
+                          ),
+                          Align(
+                            widthFactor: 0.5,
+                            heightFactor: 0.5,
+                            alignment: Alignment.bottomLeft,
+                            child: Container(
+                              height: 150,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Color(0xE688F8FF)),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              // SizedBox(
+              //   height: 100,
+              // ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'WELCOME BACK !',
+                      style: TextStyle(
+                          color: Color(0xFF007981),
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Please Enter your Details',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 50),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide:
+                                    BorderSide(color: Colors.transparent)),
+                            labelText: 'Username',
+                            labelStyle: TextStyle(
+                                color: Color(0xFF007981),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15),
+                            hintText: 'Enter username',
+                            hintStyle: TextStyle(color: Colors.grey[700])),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 50),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide:
+                                    BorderSide(color: Colors.transparent)),
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                                color: Color(0xFF007981),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15),
+                            hintText: 'Enter password',
+                            hintStyle: TextStyle(color: Colors.grey[700])),
+                      ),
+                    ),
+                    // SizedBox(
+                    //   height: 5,
+                    // ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(
+                                activeColor: Color(0xFF88F8FF),
+                                checkColor: Color(0xFF007981),
+                                value: isremember,
+                                onChanged: ((value) {
+                                  setState(() {
+                                    isremember = value;
+                                  });
+                                })),
+                            Text(
+                              'Remember me',
+                              style: TextStyle(fontSize: 13),
+                            ),
+                          ],
+                        ),
+                        InkWell(
+                          child: Text(
+                            'Forgot Password ?',
+                            style: TextStyle(
+                                fontSize: 13,
+                                decoration: TextDecoration.underline),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => Passwordchange(),
+                            ));
+                          },
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: OutlinedButton.styleFrom(
+                          backgroundColor: Color(0xFF88F8FF)),
+                      onPressed: () {},
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't Have An Account? ",
+                          style: TextStyle(
+                            fontSize: 13,
+                          ),
+                        ),
+                        InkWell(
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(
+                                fontSize: 13,
+                                decoration: TextDecoration.underline),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => signup(),
+                            ));
+                          },
+                        ),
+                      ],
+                    ))
+                  ],
+                ),
+              ),
+              // SizedBox(
+              //   height: 30,
+              // ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        // height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        child: Stack(
+                          alignment: Alignment.bottomRight,
+                          children: [
+                            Align(
+                              widthFactor: 1,
+                              heightFactor: 0.5,
+                              alignment: Alignment.topCenter,
+                              child: Container(
+                                height: 200,
+                                width: 200,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Color(0xE610F1FF)),
+                              ),
+                            ),
+                            Align(
+                              widthFactor: 0.5,
+                              heightFactor: 1,
+                              alignment: Alignment.centerLeft,
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Color(0xE688F8FF)),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
